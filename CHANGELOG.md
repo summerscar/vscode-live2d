6f5f8fdb78a3b9fd37d093f0f4848e8181f4d8ee
shenyu <shen.yus@163.com>
2018-03-23 18:23:06 +0800

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# Change Log
+All notable changes to the "live2d" extension will be documented in this file.
+
+Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
+
+## [Unreleased]
+- Initial release
---
+++ out/src/extension.js
@@ -0,0 +1,50 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+var fs = require('fs');
+var path = require('path');
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+    function writeFile(isInstall) {
+        var base = path.dirname(require.main.filename);
+        var dir = path.join(base, 'vs', 'workbench', 'electron-browser', "bootstrap");
+        var indexPath = path.join(dir, "index.html");
+        // var extensionPath = vscode.extensions.getExtension("live2d").extensionPath;
+        var extensionPath = __dirname
+        var newFilePath = null;
+        var msg1 = "";
+        var msg2 = "";
+        if (isInstall) {
+            newFilePath = path.join(extensionPath, "./../../","html", "index.html");
+            msg1 = "Install Live2d  Error!";
+            msg2 = "Install Live2d  Complete,Reset VsCode!";
+        }
+        else {
+            newFilePath = path.join(extensionPath, "./../../", "html", "index1.html");
+            msg1 = "UnInstall Live2d  Error";
+            msg2 = "UnInstall Live2d Complete,Reset  VsCode!";
+        }
+        try {
+            fs.writeFileSync(indexPath, fs.readFileSync(newFilePath));
+        }
+        catch (err) {
+            vscode.window.showInformationMessage(msg1 + err);
+        }
+        vscode.window.showInformationMessage(msg2);
+    }
+    vscode.commands.registerCommand('live2d.install', (e) => {
+        writeFile(true);
+    });
+    vscode.commands.registerCommand('live2d.uninstall', (e) => {
+        writeFile(false);
+    });
+}
+exports.activate = activate;
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
+//# sourceMappingURL=extension.js.map
